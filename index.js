7f1531b6ca698053b7762e9e90061576a0422111
jatinkray <35005537+jatinkray@users.noreply.github.com>
2020-01-23 15:41:05 +0400

    temp logs added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    console.log("this is log for request");
 });
 
 const port = process.env.PORT || 1337;
