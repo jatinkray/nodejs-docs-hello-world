9ba7a0b60cc9cb47bfdf9dbbd0fb6fdd946e687b
jatinkray <35005537+jatinkray@users.noreply.github.com>
2020-01-23 15:45:35 +0400

    added host name in log

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello Jatin!");
-    console.log("this is log for request");
+    console.log("this is log for request from host: %s", process.env.COMPUTERNAME);
 });
 
 const port = process.env.PORT || 1337;
